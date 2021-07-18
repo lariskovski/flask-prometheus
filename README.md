2ce7bb4e738df796d61bffdb75e4b8632e99e23b
lariskovski <larissaporto@live.com>
2021-07-18 05:54:02 +0000

    First commit

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # flask-prometheus
+
+## Running Locally
+
+~~~~
+pip install pipenv
+pipenv shell
+pipenv sync
+FLASK_APP=main.py flask run --host=0.0.0.0
+~~~~
+
+## Testing
+
+~~~~
+for i in {1..100}; do curl localhost:5000/test/ ; sleep 1 ; done
+~~~~
+
+## Source
---
+++ main.py
@@ -0,0 +1,26 @@
+from helpers.middleware import setup_metrics
+from flask import Flask
+
+app = Flask(__name__)
+
+setup_metrics(app)
+
+
+@app.route('/test/')
+def test():
+    return 'rest'
+
+
+@app.route('/test1/')
+def test1():
+    1/0
+    return 'rest'
+
+
+@app.errorhandler(500)
+def handle_500(error):
+    return str(error), 500
+
+
+if __name__ == '__main__':
+    app.run()
